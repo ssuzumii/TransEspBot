882c2d0f5dbf06f60003b2dce5cb7dff6c18a5dc
ssuzumii <luciamb000@gmail.com>
2022-04-14 03:37:51 +0200

    Actualización del fichero SQL con la estructura de la base de datos.

--- BD.sql
+++ BD.sql
@@ -8,6 +8,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
+CREATE TABLE IF NOT EXISTS `advertencia` (
+  `id_advertencia` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuarie` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `motivo` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_responsable` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unix_alta` bigint(20) NOT NULL,
+  `unix_baja` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id_advertencia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `temporizador_activo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_solicitante` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -21,9 +31,6 @@ CREATE TABLE IF NOT EXISTS `variable` (
   `valor` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`clave`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO `variable` (`clave`, `valor`) VALUES
-('tiempo_novates', '0');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
